d94edb8d6a9c38e64b32dc95653ff972814eb5a8
Ged <120242971+GedK0tlet@users.noreply.github.com>
2024-11-25 17:46:50 +0300

    setup

--- methodsBook.py
+++ methodsBook.py
@@ -3,6 +3,7 @@ import random
 import string
 from book import *
 
+# Добавление книги, на вход dict с полями класса книги
 def add_book(book) -> None:
     try:
         with open('data.json', 'r+') as file:
@@ -22,11 +23,13 @@ def add_book(book) -> None:
         print(e)
         print("Непредвиденная ошибка! Попробуйте снова.")
 
+# генерирование случайного id дял книги, 10 строковых символов
 def generate_random_id() -> str:
     letters_and_digits = string.ascii_letters + string.digits
     random_id = ''.join(random.choice(letters_and_digits) for _ in range(10))
     return random_id
 
+# Удаление книги, на вход строка, где написан id книги
 def delete_book(book_id) -> None:
     try:
         with open('data.json', 'r+') as file:
@@ -46,6 +49,7 @@ def delete_book(book_id) -> None:
     except Exception as e:
         print("Непредвиденная ошибка обработки")
 
+# поиск книги, на вход аргумент поиска(название, автор или год), а так же тип параметра поиска(title, author, year), вернет список dict
 def find_books(parametr, typ) -> [dict]:
     data_to_ret = []
     try:
@@ -78,6 +82,7 @@ def all_books() -> list:
         data = json.load(file)
         return data
 
+# Изменение состояния книги, на вход идут id книги и новый статус в формате bool
 def change_book_status(book_id, new_status) -> None:
     try:
         with open('data.json', 'r+') as file:
